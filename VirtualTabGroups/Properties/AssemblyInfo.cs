61f7a47ea04b763802988f90754a03e6ca44c98c
James <shaver.tech@gmail.com>
2026-05-18 17:16:34 -0700

    Scaffold solution and empty plugin project

---
+++ VirtualTabGroups/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Virtual Tab Groups")]
+[assembly: AssemblyDescription("Logical workspace manager for Notepad++: virtual groups of open documents.")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VirtualTabGroups")]
+[assembly: AssemblyCopyright("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("aa11bb22-cc33-dd44-ee55-ff6677889900")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
